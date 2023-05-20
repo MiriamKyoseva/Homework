49348fb69839a6368a9e49568f63671f77cb82ac
Miriam Kyoseva <miriam.kyoseva1@gmail.com>
2023-05-20 11:32:15 +0300

    Added missed files.

--- BoardR/src/boardr/commands/CommandConstants.java
+++
@@ -1,8 +0,0 @@
-package boardr.commands;
-
-public class CommandConstants {
-    public static final String BOARD_ITEM_CREATED =
-            "Item was created: %s";
-    public static final String ITEM_TITLE_CHANGED =
-            "\"%s\" was changed to \"%s\".";
-}
